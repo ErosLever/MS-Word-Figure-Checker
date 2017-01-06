25115792dbb767dbc4d46017ada267a8b447b52b
Eros Lever <eros.lever@gmail.com>
2017-01-06 13:02:14 +0100

    WIP

--- references_updater.py
+++ references_updater.py
@@ -1,4 +1,55 @@
 import re, zipfile
+from collections import OrderedDict
+
+def xml2txt(xml):
+	return "".join(re.findall(r'<w:t[^>]*>(.*?)</w:t>',xml))
+
+sequences = {}
+
+class Bookmark(object):
+	def __init__(self,id,name,xml):
+		self.id = int(id)
+		self.name = name
+		self.xml = xml
+		self.nested = bool(re.search(r'<w:bookmarkStart',xml))
+		self.text = xml2txt(xml)
+		
+		field_xml = re.search(r'<w:fldChar w:fldCharType="begin"/>.*?<w:fldChar w:fldCharType="end"/>',xml,re.DOTALL)
+		if field_xml:
+			instrText = re.search(r'<w:instrText[^>]*>(.*?)</w:instrText>',field_xml.group(0),re.DOTALL)
+			if instrText:
+				seq_name = re.search(r'SEQ (\w+)',instrText.group(1),re.DOTALL)
+				if seq_name:
+					self.seq_name = seq_name.group(1)
+					sequence = sequences.get(self.seq_name,[])
+					sequence.append(self)
+					self.seq_num = len(sequence)
+					sequences[self.seq_name] = sequence
+					self.correct = self.text == "%s %d" % (self.seq_name,self.seq_num)
+		if not hasattr(self,"seq_name"):
+			self.seq_name = None
+		if not hasattr(self,"seq_num"):
+			self.seq_num = 0
+		if not hasattr(self,"correct"):
+			self.correct = True
+
+	def __repr__(self):
+		return "<bookmark id=%d name=%s nested=%s seq_name=%s seq_num=%s correct=%s>%s</bookmark>" % (self.id,self.name,self.nested,self.seq_name,self.seq_num,self.correct,self.text)
+
+def find_all_bookmarks(xml):
+	bookmark_ids = re.findall(r'<w:bookmarkStart w:id="(\d+)"', xml, re.DOTALL)
+	bookmarks = []
+	for bookmark_id in bookmark_ids:
+		regex = r'<w:bookmarkStart w:id="(%s)" w:name="([^"]+)"[^>]*>(.*?)<w:bookmarkEnd w:id="%s"/>' % (bookmark_id,bookmark_id)
+		bookmark_xml = re.search(regex, xml, re.DOTALL)
+		bookmark = Bookmark(*bookmark_xml.groups())
+		bookmarks.append(bookmark)
+	return bookmarks
+
+def find_all_sequence_bookmarks(xml):
+	return filter(lambda x:x.seq_name,find_all_bookmarks(xml))
+
+def find_references(xml,)
 
 def fix_xml_references(xml):
 	bookmarks = re.findall('<w:bookmarkStart\W(.*?)<w:bookmarkEnd\W', xml, re.DOTALL)
@@ -24,3 +75,35 @@ def fix_docx_references(path):
 if __name__ == "__main__":
 	import sys
 	map(fix_docx_references,sys.argv[1:])
+
+
+'''
+      <w:bookmarkStart w:id="141" w:name="_Ref470181140"/>
+      <w:proofErr w:type="spellStart"/>
+      <w:r w:rsidRPr="007D4707">
+        <w:t>Figura</w:t>
+      </w:r>
+      <w:proofErr w:type="spellEnd"/>
+      <w:r w:rsidRPr="007D4707">
+        <w:t xml:space="preserve"> </w:t>
+      </w:r>
+      <w:r w:rsidRPr="007D4707">
+        <w:fldChar w:fldCharType="begin"/>
+      </w:r>
+      <w:r w:rsidRPr="007D4707">
+        <w:instrText xml:space="preserve"> SEQ Figura \* ARABIC </w:instrText>
+      </w:r>
+      <w:r w:rsidRPr="007D4707">
+        <w:fldChar w:fldCharType="separate"/>
+      </w:r>
+      <w:r w:rsidR="000760DB">
+        <w:rPr>
+          <w:noProof/>
+        </w:rPr>
+        <w:t>1</w:t>
+      </w:r>
+      <w:r w:rsidRPr="007D4707">
+        <w:fldChar w:fldCharType="end"/>
+      </w:r>
+      <w:bookmarkEnd w:id="141"/>
+'''
